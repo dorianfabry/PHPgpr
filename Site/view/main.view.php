b9843d9ec45a18df7882cd20fb44d93ec73a7094
CHEN Remi <chenre@hostname.iut2.upmf-grenoble.fr>
2019-10-17 11:28:31 +0200

    carousel et bootstrap Remi

--- Site/view/main.view.php
+++ Site/view/main.view.php
@@ -2,6 +2,7 @@
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="#">
   <title>BoomBox</title>
 </head>
@@ -22,21 +23,40 @@
   <a href="#"><img src="" alt=""></a>
 
 
-<section id="main">
+  <section id="main">
+    <div class="carousel" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselIndicators" data-slide-to="2"></li>
+      </ol>
 
-</section>
+      <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
 
-<footer>
-<nav>
-  <ul>
-    <li><a href="#">Nous Contacter</a></li>
-    <li><a href="#">F.A.Q</a></li>
-    <li><a href="#">Mention légales</a></li>
-    <li><a href="#">Conditions d'utilisation</a></li>
-    <li><a href="#">Qui sommes nous?</a></li>
-  </ul>
-</nav>
-</footer>
+    </div>
+  </section>
+
+  <footer>
+    <nav>
+      <ul>
+        <li><a href="#">Nous Contacter</a></li>
+        <li><a href="#">F.A.Q</a></li>
+        <li><a href="#">Mention légales</a></li>
+        <li><a href="#">Conditions d'utilisation</a></li>
+        <li><a href="#">Qui sommes nous?</a></li>
+      </ul>
+    </nav>
+  </footer>
 
 
 </body>
