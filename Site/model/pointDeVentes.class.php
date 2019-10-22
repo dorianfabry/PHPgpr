799cdad00e4b9c8665776fb18fc23374db4c3382
BERNABE Aurelien <bernabea@hostname.iut2.upmf-grenoble.fr>
2019-10-22 10:50:55 +0200

    PointDeVentes.class

---
+++ Site/model/pointDeVentes.class.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ */
+class pointDeVentes
+{
+
+  public $intitule;
+  public $adresse;
+  public $articles;
+
+
+  function __construct()
+  {
+          setIntitule($_array[0]);
+
+          setDescription($_array[1]);
+
+          setType($_array[2]);
+
+          setPrix($_array[3]);
+
+          setReference($_array[4]);
+
+          setPhoto($_array[5]);
+  }
+}
+
+
+
+
+
+
+
+ ?>
