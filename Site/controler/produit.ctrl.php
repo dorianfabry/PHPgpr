20e029ba393eb38d75a7cd2a043144edffdf4b41
Your Name <you@example.com>
2019-10-22 21:46:13 +0200

    produit

--- Site/controler/produit.ctrl.php
+++ Site/controler/produit.ctrl.php
@@ -1,5 +1,7 @@
 <?php
-$database = new articleDAO('../model/DATA')
+require_once('..\model\article.class.php');
+require_once('..\model\articleDAO.class.php');
+$database = new articleDAO('../model/DATA');
 $article = $database.get("JBL","intitule");
 
 
