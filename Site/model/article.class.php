05804f3f329d09a5f9748fcc5e5cb4e991942570
Your Name <you@example.com>
2019-10-22 21:58:21 +0200

    correction erreur article class

--- Site/model/article.class.php
+++ Site/model/article.class.php
@@ -11,39 +11,7 @@ class Article
   public $reference;
   public $photo;
 
-  public function __construct($_array)
-  {
-      setIntitule($_array[0]);
 
-      setDescription($_array[1]);
-
-      setType($_array[2]);
-
-      setPrix($_array[3]);
-
-      setReference($_array[4]);
-
-      setPhoto($_array[5]);
-  }
-
-  public function setIntitule($value){
-    $this->intitule = $value;
-  }
-  public function setDescription($value){
-    $this->description  = $value;
-  }
-  public function setType($value){
-    $this->type = $value;
-  }
-  public function setPrix($value){
-    $this->prix = $value;
-  }
-  public function setReference($value){
-    $this->reference = $value;
-  }
-  public function setPhoto($value){
-    $this->photo = $value;
-  }
   public function getIntitule(){
     return $this->intitule;
   }
