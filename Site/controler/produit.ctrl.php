952e474a2a30c19605a03d6bea772cd829bcfc69
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-10-23 17:31:50 +0200

    windows

--- Site/controler/produit.ctrl.php
+++ Site/controler/produit.ctrl.php
@@ -1,13 +1,22 @@
 <?php
+//ARTICLE
 require_once('../model/article.class.php');
 require_once('../model/articleDAO.class.php');
 $article = new Article();
 $produits = $article->load();
-//var_dump($produits);
+//var_dump($produits); TEST fonctionne vecteur d'article dans la créaton d'objet
 
-//$url1 = $produits[1][5];
 //$database = new articleDAO('../model/DATA/article.db');
 //$database->get(0,'intitule');//test de la database
+//pointDeVentes
+require_once('../model/pointDeVentes.class.php');
+require_once('../model/pointDeVentesDAO.class.php');
+$pdv = new pointDeVentes();
+$pdvs = $pdv->loadPointDeVentesDB();
+var_dump($pdvs);//Affiche vecteur de plusieurs pdvs sans la création d'objet
+
+
+//----------------Partie Rémi view------------------
 
 //include("../framework/View.class.php");
 //$view = new View("produit.view.php");
@@ -16,7 +25,7 @@ $produits = $article->load();
 //$view->show();
 
 
-//---------------------------TP4--------------------------------------------
+//---------------------------Comme le TP4--------------------------------------------
 
 // Récupération des données de configuration
 $config = parse_ini_file('../config/config.ini');
@@ -26,7 +35,7 @@ $articles = new articleDAO($config['databasearticle_path']);
 
 // Récupération de l'objet article correspondant à l'id
 $id="1";
-$res = $articles->getArticle($id);
+$res = $articles->getArticle($id); // Creation d'objet avec FETCH_CLASS
 
 
 // Sans Framework
