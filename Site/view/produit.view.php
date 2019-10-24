f00f1e4d0e326c6cc16a622044b09d0da11cc870
BERNABE Aurelien <bernabea@hostname.iut2.upmf-grenoble.fr>
2019-10-24 17:24:11 +0200

    ra

--- Site/view/produit.view.php
+++ Site/view/produit.view.php
@@ -7,10 +7,6 @@
   <body>
 
       <?php
-<<<<<<< HEAD
-=======
-
->>>>>>> 8ce6ff1122ac3fb231d6a865f31c44cfa4da04dc
        foreach($articlesvoulus as $exemple){
         $cover = $config['image_path'].'/'.$exemple->getPhoto();?>
         <a href="description.view.php?id=<?php echo $exemple->getReference() ?>"><img src="<?=$cover?>" alt=""></a>
