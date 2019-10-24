ecafdea5473c4f41ca2c8b4c70ce21568b4a75bd
BERNABE Aurelien <bernabea@hostname.iut2.upmf-grenoble.fr>
2019-10-24 18:17:01 +0200

    retour

--- Site/view/description.view.php
+++ Site/view/description.view.php
@@ -6,7 +6,7 @@
   </head>
   <body>
            <?php $cover = $config['image_path'].'/'.$article->getPhoto();?>
-            <a href="../controler/produit.ctrl.php"> <img src="img_sites/flecheRetour.png" alt=""> </a>
+            <a href="../controler/produit.ctrl.php"> <img src="img_sites/flecheRetour.png" alt="retour" width='50' heigth='50'> </a>
              <h1><?=$article->getIntitule()?>  </h1>
              <img src="<?=$cover?>" alt="">
              <p><?=$article->getDescription()?></p>
