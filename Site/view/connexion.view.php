a0efabaaa72ab24fda6ca732a41d4dd300c79f00
Your Name <dorianfabry@gmail.com>
2019-10-31 09:04:09 +0100

    connexion admin

---
+++ Site/view/connexion.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body style="text-align:center; background-color:rgba(255,69,0,0.8);">
+    <h1>Connexion </h1>
+    <p>
+      identifiant : <br>
+    <input type="text" name="identifiant" value="">
+    </p>
+    <br>
+    <p>
+      Mot de passe : <br>
+    <input type="text" name="mdp" value="">
+    </p>
+
+    <p>
+    <input type="submit" value="Envoyer" href="../view/admin.view.php" >
+    <input type="reset" value="Annuler">
+    </p>
+  </body>
+</html>
